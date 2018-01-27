f27cca7dd4b0cf50edbc580aa5ef7bedc4ace138
stanislavbagalev <stanislav.bagalev@toptal.com>
2018-01-27 14:57:35 +0200

    character animation

---
+++ Assets/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    Animator animator;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("Start Walking");
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("Stop Walking");
+        }
+    }
+}
