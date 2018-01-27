a3ca31ecf003f1d6aa309d052d421517529e990f
Dimitar <uzunov.dimitar@yahoo.com>
2018-01-27 13:36:50 +0200

    pl stats

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 
-
+[RequireComponent(typeof(PlayerBehavior))]
 public class PlayerStats : MonoBehaviour {
 
     public static PlayerStats Instance;
 
     [SerializeField] private Animator anim;
 
+    private PlayerBehavior plBeh;
+
     //TODO: make private
     public Gender playerGender = Gender.Male;
+
+    private void Start() {
+        plBeh = GetComponent<PlayerBehavior>();
+    }
 
     public void SwitchGender() {
         playerGender = playerGender.SwitchGender();
@@ -17,6 +23,11 @@ public class PlayerStats : MonoBehaviour {
 
     public Gender GetGender() {
         return playerGender;
+    }
+
+    public void EnablePlayer(bool isEnabled) {
+        if (isEnabled) plBeh.DisableInput();
+        else plBeh.EnableInput();
     }
 
     private void Awake() {
