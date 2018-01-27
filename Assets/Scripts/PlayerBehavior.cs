ba8e8b85099a522169f95975d037f3fde8582863
DESKTOP-MVO1SCC\uLDiss0x <uLDiss0x@abv.bg>
2018-01-27 16:38:12 +0200

    - New Animation added

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -107,6 +107,7 @@ public class PlayerBehavior : MonoBehaviour
             Vector3 dir = transform.parent.position - transform.position;
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.GetComponent<SpriteRenderer>().flipY = true;
         }
     }
 
