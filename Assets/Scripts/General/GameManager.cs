193c59cf5ae832283e5cefabc4dc4178c7cff28d
Dimitar <uzunov.dimitar@yahoo.com>
2018-01-27 11:32:41 +0200

    Gender switcher done; added a Game Manager script

---
+++ Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ReloadScene() {
+        OpenScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OpenScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void Quit() {
+        Application.Quit();
+    }
+
+    private void OnApplicationFocus(bool focus) {
+        if (focus) {
+            Time.timeScale = 1;
+        } else {
+            Time.timeScale = 0;
+        }
+    }
+}
