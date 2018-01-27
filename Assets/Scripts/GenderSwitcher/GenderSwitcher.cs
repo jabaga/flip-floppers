99adffc578fd005b58f8a3306dcd075a061aeff8
stanislavbagalev <stanislav.bagalev@toptal.com>
2018-01-27 23:31:26 +0200

    gender switch

--- Assets/Scripts/GenderSwitcher/GenderSwitcher.cs
+++ Assets/Scripts/GenderSwitcher/GenderSwitcher.cs
@@ -2,9 +2,11 @@
 
 public class GenderSwitcher : MonoBehaviour {
 
+    public Gender gender;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
-            other.GetComponent<PlayerStats>().SwitchGender();
+            other.GetComponent<PlayerStats>().SwitchGender(gender);
         }
     }
 }
