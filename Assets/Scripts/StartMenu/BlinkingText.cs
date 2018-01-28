057f0f9a08649415534895001c62587a7141f50b
DESKTOP-MVO1SCC\uLDiss0x <uLDiss0x@abv.bg>
2018-01-28 12:18:04 +0200

    - Start Screen added with sound

---
+++ Assets/Scripts/StartMenu/BlinkingText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingText : MonoBehaviour
+{
+    public Text Text;
+
+	void Start ()
+    {
+        InvokeRepeating("BlinkingFunc", 0.7f, 0.7f);
+	}
+
+    void BlinkingFunc()
+    {
+        if (Text.isActiveAndEnabled)
+            Text.gameObject.SetActive(false);
+        else
+            Text.gameObject.SetActive(true);
+    }
+	
+
+}
