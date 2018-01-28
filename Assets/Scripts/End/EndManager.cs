3957ffd68c900c22e140c5a92bc78c321d6b4680
DESKTOP-MVO1SCC\uLDiss0x <uLDiss0x@abv.bg>
2018-01-28 13:38:17 +0200

    - End level manager added - Animation to end levle manger added - Sound Manager level added

--- Assets/Scripts/End/EndManager.cs
+++ Assets/Scripts/End/EndManager.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using AudioHelper;
 
 public class EndManager : MonoBehaviour
 {
+    public GameObject EndCanvas;
+    public AudioClip EndSound;
+    public GameObject Player;
+
+    private void Awake()
+    {
+        EndCanvas.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
-            
+            EndCanvas.gameObject.SetActive(true);
+            AudioManager.instance.RandomizeMiscSfx(EndSound);
+            transform.gameObject.SetActive(false);
+            Player.SetActive(false);
         }
     }
 
