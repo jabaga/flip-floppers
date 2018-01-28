4602cd341215c9d2a25c844f831d86e61a97b833
DESKTOP-MVO1SCC\uLDiss0x <uLDiss0x@abv.bg>
2018-01-28 11:03:36 +0200

    - Audio sounds added

--- Assets/Scripts/GenderSwitcher/GenderSwitcher.cs
+++ Assets/Scripts/GenderSwitcher/GenderSwitcher.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
+using AudioHelper;
 
 public class GenderSwitcher : MonoBehaviour {
 
     public Gender gender;
+    public AudioClip FemaleChangeSound;
+    public AudioClip MaleChangeSound;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             other.GetComponent<PlayerStats>().SwitchGender(gender);
+
+            if (gender == Gender.Female)
+            {
+                AudioManager.instance.RandomizeMiscSfx(FemaleChangeSound);
+            }
+            else if(gender == Gender.Male)
+            {
+                AudioManager.instance.RandomizeMiscSfx(MaleChangeSound);
+            }
         }
     }
 }
