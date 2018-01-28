330082eb67f9751436a8670a0bbaf4cbbe5309cc
DESKTOP-MVO1SCC\uLDiss0x <uLDiss0x@abv.bg>
2018-01-28 13:08:38 +0200

    - End Canvas Added

---
+++ Assets/Scripts/End/EndManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EndManager : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+
+}
